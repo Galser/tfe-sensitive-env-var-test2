44bbd34e4726389097b24882636c967e6f3ad829
Andrii Guselietov <guseletov@gmail.com>
2020-10-13 16:12:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "null_resource" "check_env_var" {
     command = "echo Here it is : $SENSITIVE_ENVIRONMENT_VARIABLE "
   }
 }
+
+
+resource "null_resource" "show_env_var_on_destroy" {
+  when = destroy
+  provisioner "local-exec" {
+    command = "echo Destroy value : $SENSITIVE_ENVIRONMENT_VARIABLE "
+  }
+}
